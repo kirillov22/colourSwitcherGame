6911353f9ab7d97a5b1b72665703c3f4f8369242
Kirill Kirillov <kki43@uclive.ac.nz>
2017-09-23 20:03:12 +1200

    Trying to get buttons to change colour on click

--- app/src/main/java/me/kirillov22/colourswitchergame/MainActivity.java
+++ app/src/main/java/me/kirillov22/colourswitchergame/MainActivity.java
@@ -1,7 +1,10 @@
 package me.kirillov22.colourswitchergame;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void switchColour1 (View view) {
+        Button btn = (Button)findViewById(R.id.btnColour1);
+        btn.setBackgroundColor(Color.parseColor("#80CBC4"));
+    }
+
+    public void switchColour2 (View view) {
+        Button btn = (Button)findViewById(R.id.btnColour2);
+        btn.setBackgroundColor(Color.parseColor("#C5E1A5"));
     }
 }
